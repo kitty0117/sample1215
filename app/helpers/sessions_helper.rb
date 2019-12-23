46f32e6e4c171c05a48341c47d81541fc5fc471d
kitty0117 <axx_ra5882@yahoo.co.jp>
2019-12-23 01:38:33 +0000

    Finish user edit update index and destroyactions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -48,6 +48,21 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end 
+  
+  def current_user?(user)
+    user == current_user
+  end 
+  
+  #記憶したurl(もしくはdefault値)にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end 
+  
+  #アクセスしようとしていたURLを覚えておく
+  def store_location
+    session[:forwarding_url]= request.original_url if request.get?
+  end 
 
 
 end
---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  def setup
+    @admin=users(:michael)
+    @other=users(:archer)
+  end 
+  
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users= User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user),text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end 
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@other)
+    end
+  end
+  
+  test "index as non admin" do
+    log_in_as(@other)
+    assert_no_difference 'User.count' do
+      delete user_path(@admin)
+    end 
+  end 
+  
+  
+end
